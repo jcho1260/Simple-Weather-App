f0859d9208835782689a74afb7e2f690345b61e2
Jin Cho <jin.cho098@duke.edu>
2020-02-12 15:47:53 -0500

    done?

---
+++ hw2/hw2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw2
+//
+//  Created by Jin Cho on 2/12/20.
+//  Copyright © 2020 Jin Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
